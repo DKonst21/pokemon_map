953e8d6a831feabbb8717e3f7fed2960cdec0192
DKonst21 <104372191+DKonst21@users.noreply.github.com>
2023-04-08 15:19:23 +0400

    Changed default settings for PokemonEntity fields

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Pokemon(models.Model):
     title = models.CharField('Название (рус.)', max_length=200)
-    title_en = models.CharField('Название (англ.)', max_length=200)
-    title_jp = models.CharField('Название (яп.)', max_length=200)
+    title_en = models.CharField('Название (англ.)', max_length=200, blank=True)
+    title_jp = models.CharField('Название (яп.)', max_length=200, blank=True)
     image = models.ImageField(upload_to='pictures', null=True, blank=True, verbose_name='Изображение покемона')
     description = models.TextField('Описание', blank=True)
     previous_evolution = models.ForeignKey(
@@ -27,15 +27,19 @@ class Pokemon(models.Model):
 class PokemonEntity(models.Model):
     latitude = models.FloatField(verbose_name='Широта')
     longitude = models.FloatField(verbose_name='Долгота')
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name="name_pokemon",
+    pokemon = models.ForeignKey("Pokemon",
+                                on_delete=models.SET_NULL,
+                                null=True,
+                                blank=True,
+                                related_name="name_pokemon",
                                 verbose_name='Имя покемона')
     appeared_at = models.DateTimeField(null=True, verbose_name='Время появления')
     disappeared_at = models.DateTimeField(null=True, verbose_name='Время исчезновения')
-    level = models.IntegerField(default=0, verbose_name='Уровень')
-    health = models.IntegerField(default=0, verbose_name='Здоровье')
-    strength = models.IntegerField(default=0, verbose_name='Сила')
-    defence = models.IntegerField(default=0, verbose_name='Зашита')
-    stamina = models.IntegerField(default=0, verbose_name='Выносливость')
+    level = models.IntegerField(blank=True, null=True, verbose_name='Уровень')
+    health = models.IntegerField(blank=True, null=True, verbose_name='Здоровье')
+    strength = models.IntegerField(blank=True, null=True, verbose_name='Сила')
+    defence = models.IntegerField(blank=True, null=True, verbose_name='Зашита')
+    stamina = models.IntegerField(blank=True, null=True, verbose_name='Выносливость')
 
     def __str__(self):
         return f"{self.pokemon}: {self.latitude}, {self.longitude}"
